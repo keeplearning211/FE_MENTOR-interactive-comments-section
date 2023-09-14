f6e4de09682089c38be307c2f4d3476c239050ae
Dong <dong211cloudinarytraining@gmail.com>
2023-09-15 00:58:54 +0700

    add confirm modal

---
+++ src/components/DeleteConfirmModal/index.tsx
@@ -0,0 +1,116 @@
+import { useState, useEffect, useRef } from 'react';
+
+const ESC_KEY = 'Escape'
+
+interface ConfirmModalProps {
+  title: string;
+  message: string;
+  onConfirm: () => void;
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+const DeleteConfirmModal = ({ title, message, onConfirm, onClose, isOpen }: ConfirmModalProps) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const modalContainerRef = useRef<HTMLDivElement>(null)
+  const cancelBtnRef = useRef<HTMLButtonElement>(null)
+  const confirmBtnRef = useRef<HTMLButtonElement>(null)
+
+  const [bodyCurrentPosition] = useState(window.scrollY)
+
+  // close modal when clicking outside modal
+  const outSideModalClickHandler = (event: Event) => {
+    if (modalRef.current && !(modalRef.current as HTMLDivElement).contains(event.target as HTMLDivElement)) {
+      handleClose()
+    }
+  }
+
+  // close modal when pressing ESCAPE
+  const EscKeydownHandler = (event: KeyboardEvent) => {
+    if (event.key === ESC_KEY) {
+      handleClose()
+    }
+  }
+
+  const modalRef = useRef(null)
+  useEffect(() => {
+    document.addEventListener('mousedown', outSideModalClickHandler)
+    document.addEventListener('keydown', EscKeydownHandler)
+    return () => {
+      document.removeEventListener('keydown', EscKeydownHandler)
+      document.removeEventListener('mousedown', outSideModalClickHandler)
+    }
+  })
+
+  // set focus to cancel button and prevent the modal from closing
+  useEffect(() => {
+    setTimeout(() => {
+      cancelBtnRef.current?.focus()
+    }, 150)
+
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${bodyCurrentPosition}px`;
+    return () => {
+      document.body.style.position = '';
+      document.body.style.top = '';
+      window.scrollTo(0, bodyCurrentPosition)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (modalContainerRef.current) {
+      modalContainerRef.current.addEventListener('keydown', (e: KeyboardEvent) => {
+        if (e.key === 'Tab') {
+          if (e.shiftKey) /* shift + tab */ {
+            if (document.activeElement === cancelBtnRef.current) {
+              confirmBtnRef.current?.focus();
+              e.preventDefault();
+            }
+          } else /* tab */ {
+            if (document.activeElement === confirmBtnRef.current) {
+              cancelBtnRef.current?.focus();
+              e.preventDefault();
+            }
+          }
+        }
+      })
+    }
+
+  }, [modalRef])
+
+
+  const handleClose = () => {
+    console.log('handleClose');
+
+    setIsModalOpen(false);
+    onClose()
+  };
+
+  const handleConfirm = () => {
+    setIsModalOpen(false);
+    onConfirm();
+  };
+
+  useEffect(() => {
+    setIsModalOpen(isOpen)
+  }, [isOpen])
+
+  return (
+    <div
+      ref={modalContainerRef}
+      role="dialog"
+      className={`modal-container${isModalOpen ? '' : ' hidden'}`}
+    >
+      <div className="modal" ref={modalRef}>
+        <h1 className="title">{title}</h1>
+        <p className="content">{message}</p>
+        <div className="action">
+          <button ref={cancelBtnRef} className="cancel-btn" onClick={handleClose}>NO, CANCEL</button>
+          <button ref={confirmBtnRef} className="delete-confirm-btn" onClick={handleConfirm}> YES, DELETE</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteConfirmModal
--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -3,6 +3,7 @@ import Avatar from '../Avatar';
 import { User } from '../../type';
 import Data from '../../data.json';
 import Compose from '../Compose';
+import DeleteConfirmModal from '../DeleteConfirmModal';
 
 interface CommentProps {
   id: number;
@@ -18,6 +19,7 @@ interface CommentProps {
 
 function Comment({ content, user, score, createdAt, replies, replyingTo = '', isReply, replyHandler }: CommentProps) {
   const [replying, setReplying] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const { currentUser: { username: currentUser } } = Data
   const hasReplies = replies?.length > 0;
@@ -42,55 +44,74 @@ function Comment({ content, user, score, createdAt, replies, replyingTo = '', is
     }
   }, [composeRef])
 
+  const confirmDeleteHandler = () => {
+    setIsModalOpen(false)
+  }
+
+  const closeConfirmModalHandler = () => {
+    setIsModalOpen(false)
+  }
+
+  const onClickDeleteHandler = () => {
+    setIsModalOpen(true)
+  }
 
   return (
-    <div className={`comment${hasReplies ? ' has-replies' : ''}${replying ? ' replying' : ''}`}>
-      <div className="author">
-        <Avatar username={user?.username} />
-        <p className="name">
-          {user?.username}
-          {currentUser === user?.username && <span className="you-tag">You</span>}
+    <>
+      <div className={`comment${hasReplies ? ' has-replies' : ''}${replying ? ' replying' : ''}`}>
+        <div className="author">
+          <Avatar username={user?.username} />
+          <p className="name">
+            {user?.username}
+            {currentUser === user?.username && <span className="you-tag">You</span>}
+          </p>
+          <p className="timestamps">{createdAt}</p>
+        </div>
+        <p className="content">
+          {replyingTo && <span className="replying-to">@{replyingTo}</span>} {content}
         </p>
-        <p className="timestamps">{createdAt}</p>
-      </div>
-      <p className="content">
-        {replyingTo && <span className="replying-to">@{replyingTo}</span>} {content}
-      </p>
-      <div className="score">
-        <button className="add-btn" />
-        {score}
-        <button className="minus-btn" />
-      </div>
-      <div className="action">
+        <div className="score">
+          <button className="add-btn" />
+          {score}
+          <button className="minus-btn" />
+        </div>
+        <div className="action">
+          {
+            currentUser !== user?.username ?
+              <button className="reply-btn" onClick={replyClickHandler}><i className="reply-icon"></i> Reply</button> :
+              <>
+                <button className="delete-btn" onClick={onClickDeleteHandler}><i className="delete-icon"></i>Delete</button>
+                <button className="edit-btn"><i className="edit-icon"></i>Edit</button>
+              </>
+          }
+        </div>
         {
-          currentUser !== user?.username ?
-            <button className="reply-btn" onClick={replyClickHandler}><i className="reply-icon"></i> Reply</button> :
-            <>
-              <button className="delete-btn"><i className="delete-icon"></i>Delete</button>
-              <button className="edit-btn"><i className="edit-icon"></i>Edit</button>
-            </>
-        }
-      </div>
-      {
-        hasReplies && (
-          <div className="reply">
-            <span className="line"></span>
-            <div className="comments">
-              {
-                replies.map((comment) => (
-                  <Comment key={comment.id}
-                    {...comment}
-                    isReply={true}
-                    replyHandler={replyClickHandler}
-                  />
-                ))
-              }
+          hasReplies && (
+            <div className="reply">
+              <span className="line"></span>
+              <div className="comments">
+                {
+                  replies.map((comment) => (
+                    <Comment key={comment.id}
+                      {...comment}
+                      isReply={true}
+                      replyHandler={replyClickHandler}
+                    />
+                  ))
+                }
+              </div>
             </div>
-          </div>
-        )
-      }
-      {replying && <Compose ref={composeRef} replying={replying} username={currentUser} />}
-    </div>
+          )
+        }
+        {replying && <Compose ref={composeRef} replying={replying} username={currentUser} />}
+      </div>
+      {isModalOpen && <DeleteConfirmModal
+        title="Delete comment"
+        message="Are you sure you want to delete this comment? This will remove the comment and can’t be undone."
+        onConfirm={confirmDeleteHandler}
+        onClose={closeConfirmModalHandler}
+        isOpen={isModalOpen} />}
+    </>
   )
 }
 
