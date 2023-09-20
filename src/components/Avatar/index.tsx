a885f6a670dd4f839f71300cd948aaa869622419
Dong <dong211cloudinarytraining@gmail.com>
2023-09-20 12:14:17 +0700

    add default avatar image

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,6 +1,6 @@
 
 function Avatar({ username }: { username: string }) {
-  return <img className="avatar" src={`src/assets/images/avatars/image-${username}.webp`} />
+  return <img className="avatar" src={`src/assets/images/avatars/image-${username ?? 'default'}.png`} />
 
 }
 
