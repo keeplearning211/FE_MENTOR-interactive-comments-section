2a9630c0ecd8de1b943c9cd845fb41f2262f7ee2
Dong <dong211cloudinarytraining@gmail.com>
2023-09-21 14:42:30 +0700

    Static Asset Handling

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,6 +1,27 @@
+import amyrobsonImgSrc from '../../assets/images/avatars/image-amyrobson.png'
+import juliusomoImgSrc from '../../assets/images/avatars/image-juliusomo.png'
+import maxblagunImgSrc from '../../assets/images/avatars/image-maxblagun.png'
+import ramsesmironImgSrc from '../../assets/images/avatars/image-ramsesmiron.png'
+import defaultImgSrc from '../../assets/images/avatars/image-default.png'
+
+function getAvatarSrc(username: string): string {
+  switch (username) {
+    case 'amyrobson':
+      return amyrobsonImgSrc
+    case 'juliusomo':
+      return juliusomoImgSrc
+    case 'maxblagun':
+      return maxblagunImgSrc
+    case 'ramsesmiron':
+      return ramsesmironImgSrc
+    default:
+      return defaultImgSrc
+  }
+}
 
 function Avatar({ username }: { username: string }) {
-  return <img className="avatar" src={`src/assets/images/avatars/image-${username ?? 'default'}.png`} />
+  const imgSrc = getAvatarSrc(username)
+  return <img className="avatar" src={imgSrc} />
 
 }
 
