fbdfe184e9e72878b71a77dc6f9dd774c1b8f6ce
Dong <dong211cloudinarytraining@gmail.com>
2023-09-22 16:17:06 +0700

    increase lighthouse score

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,34 +1,32 @@
-import amyrobsonImgSrc from '../../assets/images/avatars/image-amyrobson.png'
-import juliusomoImgSrc from '../../assets/images/avatars/image-juliusomo.png'
-import maxblagunImgSrc from '../../assets/images/avatars/image-maxblagun.png'
-import ramsesmironImgSrc from '../../assets/images/avatars/image-ramsesmiron.png'
-import defaultImgSrc from '../../assets/images/avatars/image-default.png'
+// import amyrobsonImgSrc from '../../assets/images/avatars/image-amyrobson.png'
+// import juliusomoImgSrc from '../../assets/images/avatars/image-juliusomo.png'
+// import maxblagunImgSrc from '../../assets/images/avatars/image-maxblagun.png'
+// import ramsesmironImgSrc from '../../assets/images/avatars/image-ramsesmiron.png'
+// import defaultImgSrc from '../../assets/images/avatars/image-default.png'
 
-function getAvatarSrc(username: string): string {
-  switch (username) {
-    case 'amyrobson':
-      return amyrobsonImgSrc
-    case 'juliusomo':
-      return juliusomoImgSrc
-    case 'maxblagun':
-      return maxblagunImgSrc
-    case 'ramsesmiron':
-      return ramsesmironImgSrc
-    default:
-      return defaultImgSrc
-  }
-}
+// function getAvatarSrc(username: string): string {
+//   switch (username) {
+//     case 'amyrobson':
+//       return amyrobsonImgSrc
+//     case 'juliusomo':
+//       return juliusomoImgSrc
+//     case 'maxblagun':
+//       return maxblagunImgSrc
+//     case 'ramsesmiron':
+//       return ramsesmironImgSrc
+//     default:
+//       return defaultImgSrc
+//   }
+// }
 
-// function Avatar({ username }: { username: string }) {
+// function Avatar({ username, size = 32 }: { username: string, size?: number }) {
 //   const imgSrc = getAvatarSrc(username)
-//   return <img className="avatar" src={imgSrc} alt="avatar image" />
+//   return <img className="avatar" src={imgSrc} alt="avatar image" width={size} height={size} />
 
 // }
 
-function Avatar({ username, size = 32 }: { username: string, size?: number }) {
-  const imgSrc = getAvatarSrc(username)
-  return <img className="avatar" src={imgSrc} alt="avatar image" width={size} height={size} />
-
+function Avatar({ username }: { username: string }) {
+  return <i className="avatar" data-username={username}></i>
 }
 
 export default Avatar
